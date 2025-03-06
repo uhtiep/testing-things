b70d206a6d9ef36a170f104b61c9ce0e12918c88
uhtiep <gojo_king@Outlook.com>
2025-03-06 01:29:11 -0500

    PEAK

--- java.js
+++ java.js
@@ -1,3 +1,3 @@
 let a = 21;
 let miguel = gay;
-document.getElementById("p1").textContent = "the number ${a} can be ${miguel}";
+document.getElementById("p1").textContent = `the number ${a} can be ${miguel}`;
