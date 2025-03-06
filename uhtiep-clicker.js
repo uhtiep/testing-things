4888e4be7547138682ca5092496243feb098d521
uhtiep <gojo_king@Outlook.com>
2025-03-06 17:52:05 -0500

    commit

--- uhtiep-clicker.js
+++ uhtiep-clicker.js
@@ -1,9 +1,15 @@
-/* uhtiep-clicker.js */
 let shells = 0;
 let shellsPerClick = 1;
 let shellsPerSecond = 0;
 let rebirths = 0;
 let rebirthCost = 50000;
+let spcUpgrades = [];
+let spsUpgrades = [];
+
+for (let i = 1; i <= 25; i++) {
+    spcUpgrades.push({ name: `SPC Upgrade ${i}`, cost: i * 100, bonus: i });
+    spsUpgrades.push({ name: `SPS Upgrade ${i}`, cost: i * 150, bonus: i });
+}
 
 function clickShell() {
     shells += shellsPerClick;
@@ -46,3 +52,41 @@ function rebirth() {
         document.getElementById('rebirthBtn').innerText = `Rebirth (Cost: ${rebirthCost} shells)`;
     }
 }
+
+function buyUpgrade(type, index) {
+    let upgrade = type === 'spc' ? spcUpgrades[index] : spsUpgrades[index];
+    if (shells >= upgrade.cost) {
+        shells -= upgrade.cost;
+        upgrade.cost = Math.floor(upgrade.cost * 1.5);
+        if (type === 'spc') {
+            shellsPerClick += upgrade.bonus;
+        } else {
+            shellsPerSecond += upgrade.bonus;
+        }
+        document.getElementById('shellCount').innerText = `Uhtiep Shells: ${shells}`;
+        renderUpgrades();
+    }
+}
+
+function renderUpgrades() {
+    let spcDiv = document.getElementById('spcItems');
+    let spsDiv = document.getElementById('spsItems');
+    spcDiv.innerHTML = '';
+    spsDiv.innerHTML = '';
+
+    spcUpgrades.forEach((upgrade, index) => {
+        let btn = document.createElement('button');
+        btn.innerText = `${upgrade.name} (Cost: ${upgrade.cost}, Bonus: +${upgrade.bonus})`;
+        btn.onclick = () => buyUpgrade('spc', index);
+        spcDiv.appendChild(btn);
+    });
+
+    spsUpgrades.forEach((upgrade, index) => {
+        let btn = document.createElement('button');
+        btn.innerText = `${upgrade.name} (Cost: ${upgrade.cost}, Bonus: +${upgrade.bonus}/s)`;
+        btn.onclick = () => buyUpgrade('sps', index);
+        spsDiv.appendChild(btn);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderUpgrades);
