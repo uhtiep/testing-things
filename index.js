ced0bb407b2e50ef6c4f69676cdb2f47f0fc81f9
uhtiep <gojo_king@Outlook.com>
2025-03-06 00:12:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-window.alert("this works ong");
+document.GetElementById("info").textContent = "what should I make this site about"
+document.GetElementById("idek").textContent = "I'll sometime finish this"
