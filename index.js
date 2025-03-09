b331d322cef6b87c6de2f25e199b90a450bce61d
uhtiep <gojo_king@Outlook.com>
2025-03-09 16:02:04 -0400

    10

--- index.js
+++ index.js
@@ -3,20 +3,50 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 500;
 
-let balls = [];
 let glasses = [];
-let gravity = 0.5;
-let friction = 0.8;
 let selectedBall = null;
 
-class Ball {
-    constructor(x, y, color) {
+class Glass {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.radius = 20;
+        this.width = 60;
+        this.height = 150;
+        this.balls = [];
+    }
+    draw() {
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        this.balls.forEach((ball, index) => {
+            ball.x = this.x + this.width / 2;
+            ball.y = this.y + this.height - (index + 1) * 40;
+            ball.draw();
+        });
+    }
+    canAddBall(ball) {
+        return this.balls.length < 4 && (this.balls.length === 0 || this.balls[this.balls.length - 1].color === ball.color);
+    }
+    addBall(ball) {
+        if (this.canAddBall(ball)) {
+            this.balls.push(ball);
+            return true;
+        }
+        return false;
+    }
+    removeBall() {
+        return this.balls.pop();
+    }
+    isSorted() {
+        return this.balls.length === 4 && this.balls.every(ball => ball.color === this.balls[0].color);
+    }
+}
+
+class Ball {
+    constructor(color) {
         this.color = color;
-        this.vx = Math.random() * 4 - 2;
-        this.vy = Math.random() * 4 - 2;
+        this.radius = 20;
+        this.x = 0;
+        this.y = 0;
     }
     draw() {
         ctx.beginPath();
@@ -25,126 +55,64 @@ class Ball {
         ctx.fill();
         ctx.closePath();
     }
-    update() {
-        if (!selectedBall) {
-            this.vy += gravity;
-            this.x += this.vx;
-            this.y += this.vy;
+}
 
-            if (this.y + this.radius > canvas.height) {
-                this.y = canvas.height - this.radius;
-                this.vy *= -friction;
+function setupGame() {
+    glasses = [new Glass(100, 200), new Glass(200, 200), new Glass(300, 200), new Glass(400, 200)];
+    let colors = ["red", "blue", "green", "yellow"];
+    let allBalls = [];
+    colors.forEach(color => {
+        for (let i = 0; i < 4; i++) {
+            allBalls.push(new Ball(color));
+        }
+    });
+    allBalls.sort(() => Math.random() - 0.5);
+    
+    let index = 0;
+    glasses.forEach(glass => {
+        while (glass.balls.length < 4 && index < allBalls.length) {
+            glass.addBall(allBalls[index]);
+            index++;
+        }
+    });
+}
+
+canvas.addEventListener("click", (e) => {
+    let rect = canvas.getBoundingClientRect();
+    let mouseX = e.clientX - rect.left;
+    let mouseY = e.clientY - rect.top;
+    
+    let clickedGlass = glasses.find(glass => mouseX > glass.x && mouseX < glass.x + glass.width && mouseY > glass.y && mouseY < glass.y + glass.height);
+    
+    if (clickedGlass) {
+        if (selectedBall) {
+            if (clickedGlass.addBall(selectedBall)) {
+                selectedBall = null;
             }
-            if (this.x - this.radius < 0 || this.x + this.radius > canvas.width) {
-                this.vx *= -friction;
-            }
-            balls.forEach(ball => {
-                if (ball !== this && detectCollision(this, ball)) {
-                    resolveCollision(this, ball);
-                }
-            });
+        } else {
+            selectedBall = clickedGlass.removeBall();
         }
     }
-}
+    checkWin();
+});
 
-class Glass {
-    constructor(x, y, color) {
-        this.x = x;
-        this.y = y;
-        this.width = 50;
-        this.height = 80;
-        this.color = color;
+function checkWin() {
+    if (glasses.every(glass => glass.isSorted() || glass.balls.length === 0)) {
+        alert("You win!");
+        setupGame();
     }
-    draw() {
-        ctx.strokeStyle = "black";
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, 10);
-    }
-}
-
-function detectCollision(ball1, ball2) {
-    let dx = ball1.x - ball2.x;
-    let dy = ball1.y - ball2.y;
-    let distance = Math.sqrt(dx * dx + dy * dy);
-    return distance < ball1.radius + ball2.radius;
-}
-
-function resolveCollision(ball1, ball2) {
-    let angle = Math.atan2(ball2.y - ball1.y, ball2.x - ball1.x);
-    let speed1 = Math.sqrt(ball1.vx * ball1.vx + ball1.vy * ball1.vy);
-    let speed2 = Math.sqrt(ball2.vx * ball2.vx + ball2.vy * ball2.vy);
-    
-    let direction1 = Math.atan2(ball1.vy, ball1.vx);
-    let direction2 = Math.atan2(ball2.vy, ball2.vx);
-    
-    let velocityX1 = speed1 * Math.cos(direction1 - angle);
-    let velocityY1 = speed1 * Math.sin(direction1 - angle);
-    let velocityX2 = speed2 * Math.cos(direction2 - angle);
-    let velocityY2 = speed2 * Math.sin(direction2 - angle);
-    
-    let finalVelocityX1 = velocityX2;
-    let finalVelocityX2 = velocityX1;
-    
-    ball1.vx = Math.cos(angle) * finalVelocityX1 + Math.cos(angle + Math.PI / 2) * velocityY1;
-    ball1.vy = Math.sin(angle) * finalVelocityX1 + Math.sin(angle + Math.PI / 2) * velocityY1;
-    ball2.vx = Math.cos(angle) * finalVelocityX2 + Math.cos(angle + Math.PI / 2) * velocityY2;
-    ball2.vy = Math.sin(angle) * finalVelocityX2 + Math.sin(angle + Math.PI / 2) * velocityY2;
 }
 
 function drawObjects() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     glasses.forEach(glass => glass.draw());
-    balls.forEach(ball => {
-        ball.update();
-        ball.draw();
-    });
+    if (selectedBall) selectedBall.draw();
 }
-
-document.getElementById("addBall").addEventListener("click", () => {
-    let colors = ["red", "blue", "green", "yellow", "purple"];
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    let newBall;
-    do {
-        newBall = new Ball(Math.random() * (canvas.width - 40) + 20, Math.random() * (canvas.height - 40) + 20, color);
-    } while (balls.some(ball => detectCollision(ball, newBall)));
-    balls.push(newBall);
-});
-
-document.getElementById("addGlass").addEventListener("click", () => {
-    let colors = ["red", "blue", "green", "yellow", "purple"];
-    let color = colors[Math.floor(Math.random() * colors.length)];
-    glasses.push(new Glass(Math.random() * canvas.width, Math.random() * canvas.height, color));
-});
-
-canvas.addEventListener("mousedown", (e) => {
-    let rect = canvas.getBoundingClientRect();
-    let mouseX = e.clientX - rect.left;
-    let mouseY = e.clientY - rect.top;
-    balls.forEach(ball => {
-        let dx = mouseX - ball.x;
-        let dy = mouseY - ball.y;
-        if (Math.sqrt(dx * dx + dy * dy) < ball.radius) {
-            selectedBall = ball;
-        }
-    });
-});
-
-canvas.addEventListener("mousemove", (e) => {
-    if (selectedBall) {
-        let rect = canvas.getBoundingClientRect();
-        selectedBall.x = e.clientX - rect.left;
-        selectedBall.y = e.clientY - rect.top;
-    }
-});
-
-canvas.addEventListener("mouseup", () => {
-    selectedBall = null;
-});
 
 function animate() {
     requestAnimationFrame(animate);
     drawObjects();
 }
 
+setupGame();
 animate();
